c87a1dfd9733148a040afcb471c1e14f15e56542
Elisa <elisadiazartcontact@gmail.com>
2023-03-01 21:54:37 +0100

    01.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
-
-sayHello();
 
 const container = document.getElementById('container');
+const menuIcon = document.getElementById('menuIcon')
+const menu = document.getElementById('menu')
 
 window.addEventListener('load', () => {
   container.classList.add('container--show');
 });
+
+
+menuIcon.addEventListener('click', () => {
+ menu.classList.toggle('index--active')
+ menuIcon.classList.toggle('line--cross')
+})
