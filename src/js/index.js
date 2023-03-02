c3841e3a988928bd78b50ae89f56d5cf27702d27
Elisa <elisadiazartcontact@gmail.com>
2023-03-02 16:15:01 +0100

    02.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,15 @@
 import '../scss/styles.scss';
 
 const container = document.getElementById('container');
-const menuIcon = document.getElementById('menuIcon')
-const menu = document.getElementById('menu')
+const menuIcon = document.getElementById('menuIcon');
+const menu = document.getElementById('menu');
+const menuIconContainer = document.getElementById('menuIconContainer');
 
 window.addEventListener('load', () => {
   container.classList.add('container--show');
 });
 
-
-menuIcon.addEventListener('click', () => {
- menu.classList.toggle('index--active')
- menuIcon.classList.toggle('line--cross')
-})
+menuIconContainer.addEventListener('click', () => {
+  menu.classList.toggle('index--active');
+  menuIcon.classList.toggle('line--cross');
+});
