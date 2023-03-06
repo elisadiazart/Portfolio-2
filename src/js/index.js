1509f2909804d0a1d681e0bf23ac3baafde996ad
Elisa <elisadiazartcontact@gmail.com>
2023-03-06 22:24:46 +0100

    06.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -9,12 +9,13 @@ const galleryNav = document.getElementById('galleryNav')
 const galleryText = document.getElementById('galleryText')
 const galleryImage = document.getElementById('galleryImage')
 const galleryTitle = document.getElementById('galleryTitle')
+const galleryLiveLink = document.getElementById('pageLive')
 
 const galleryContent = {
   button1: {
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'PASSWORD GENERATOR.',
-    imagesrc: 'assets/images/item-gallery1.png'
+    imagesrc: 'assets/images/item-gallery1.png',
   } ,
   button2: {
     text: '2Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
@@ -24,7 +25,8 @@ const galleryContent = {
   button3: {
     text: '3Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'ROCK, PAPER, SCISSORS GAME.',
-    imagesrc: 'assets/images/item-gallery3.png'
+    imagesrc: 'assets/images/item-gallery3.png',
+    pageLiveLink: 'https://elisadiazart.github.io/rock-paper-scisssors-game/'
   } ,
   button4: {
     text: '4Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
@@ -57,12 +59,19 @@ menuIconContainer.addEventListener('click', () => {
   menuIcon.classList.toggle('line--cross');
 });
 
-const buttonSelected = (data)=> `button${data}`
+
 
 galleryNav.addEventListener('click', e => {
-  if(e.target.classList.contains('gallery__button')){
-    galleryText.textContent = galleryContent[buttonSelected(e.target.dataset.number)].text;
-    galleryImage.src = galleryContent[buttonSelected(e.target.dataset.number)].imagesrc
-    galleryTitle.textContent = galleryContent[buttonSelected(e.target.dataset.number)].title
+  const buttonSelected = (data)=> `button${data}`
+  console.log(e.target);
+  if(e.target.classList.contains('gallery__button-link')){
+    galleryText.textContent = galleryContent[buttonSelected(e.target.children[0].dataset.number)].text;
+    galleryImage.src = galleryContent[buttonSelected(e.target.children[0].dataset.number)].imagesrc
+    galleryTitle.textContent = galleryContent[buttonSelected(e.target.children[0].dataset.number)].title
+    galleryLiveLink.href = galleryContent[buttonSelected(e.target.children[0].dataset.number)].pageLiveLink
+    for (let index = 0; index <= 7; index++) {
+      galleryTitle.classList.remove(`project__title--${index}`)
+    }
+    galleryTitle.classList.add(`project__title--${e.target.children[0].dataset.number}`);
   }
 })
