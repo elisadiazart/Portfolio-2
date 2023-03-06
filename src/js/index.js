a8ff75da95577a5b83b5dd13562d9ffbdfe14eba
Elisa <elisadiazartcontact@gmail.com>
2023-03-07 00:57:37 +0100

    07.03.23

--- src/js/index.js
+++ src/js/index.js
@@ -10,43 +10,51 @@ const galleryText = document.getElementById('galleryText')
 const galleryImage = document.getElementById('galleryImage')
 const galleryTitle = document.getElementById('galleryTitle')
 const galleryLiveLink = document.getElementById('pageLive')
+const galleryCode = document.getElementById('code')
 
 const galleryContent = {
   button1: {
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'PASSWORD GENERATOR.',
     imagesrc: 'assets/images/item-gallery1.png',
+    code: '#'
   } ,
   button2: {
     text: '2Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'BOOKING WEBSITE.',
-    imagesrc: 'assets/images/item-gallery2.png'
+    imagesrc: 'assets/images/item-gallery2.png',
+    code: ''
   } ,
   button3: {
     text: '3Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'ROCK, PAPER, SCISSORS GAME.',
     imagesrc: 'assets/images/item-gallery3.png',
-    pageLiveLink: 'https://elisadiazart.github.io/rock-paper-scisssors-game/'
+    pageLiveLink: 'https://elisadiazart.github.io/rock-paper-scisssors-game/',
+    code: 'https://github.com/elisadiazart/rock-paper-scisssors-game'
   } ,
   button4: {
     text: '4Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'WORDLE.',
-    imagesrc: 'assets/images/item-gallery4.png'
+    imagesrc: 'assets/images/item-gallery4.png',
+    code: 'https://github.com/elisadiazart/Wordle-clone'
   },
   button5: {
     text: '5Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'QUIZ GAME.',
-    imagesrc: 'assets/images/item-gallery5.png'
+    imagesrc: 'assets/images/item-gallery5.png',
+    code:''
   },
   button6: {
     text: '6Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'POKEMON MEMORY CARD GAME.',
-    imagesrc: 'assets/images/item-gallery6.png'
+    imagesrc: 'assets/images/item-gallery6.png',
+    code:'https://github.com/elisadiazart/pokemon-cards-game'
   },
   button7: {
     text: '7Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at mollis risus. Morbi vel auctor metus, at iaculis sapien. Aliquam ornare, neque at gravida hendrerit, massa nunc porta mauris, ac ultrices leo diam vel nisl. Sed porta sit amet mauris eget suscipit. Aenean eleifend gravida lectus in porta. Quisque sit amet magna nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut at sagittis tortor. Morbi in dapibus magna. Mauris ac nulla rhoncus, lobortis nisi eget, facilisis felis. Aliquam in neque eu lectus malesuada accumsan sed non nunc. Quisque et elit quam.',
     title: 'TO DO LIST APP.',
-    imagesrc: 'assets/images/item-gallery7.png'
+    imagesrc: 'assets/images/item-gallery7.png',
+    code:'https://github.com/elisadiazart/To-do-list-practice'
   },
 
 };
@@ -69,6 +77,7 @@ galleryNav.addEventListener('click', e => {
     galleryImage.src = galleryContent[buttonSelected(e.target.children[0].dataset.number)].imagesrc
     galleryTitle.textContent = galleryContent[buttonSelected(e.target.children[0].dataset.number)].title
     galleryLiveLink.href = galleryContent[buttonSelected(e.target.children[0].dataset.number)].pageLiveLink
+    galleryCode.href = galleryContent[buttonSelected(e.target.children[0].dataset.number)].code
     for (let index = 0; index <= 7; index++) {
       galleryTitle.classList.remove(`project__title--${index}`)
     }
